25a0f10a61bbcba5719549747e76705339a96082
marcelogturing <marcelo.g@turing.com>
2025-09-02 14:59:26 -0300

    Update counter.java

--- counter.java
+++ counter.java
@@ -1,11 +1,11 @@
 class Counter {
     private int count = 0;
 
-    public void increment() {
-        count++;
-    }
-    
     public int getCount() {
         return count;
+    }
+    
+    public void increment() {
+        count++;
     }
 }
